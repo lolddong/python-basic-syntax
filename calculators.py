700da288331c13ed10be5a4d9e8e80ffc2fd381d
lolddong <w2crosss@gmail.com>
2023-05-22 17:19:41 +0900

    third

--- calculators.py
+++ calculators.py
@@ -3,18 +3,18 @@ b = 4
 # '3'와 '4'는 정수, '0.75'는 실수
 # 파이썬은 정수를 가지고 계산해서 실수 값이 나와도 자동적으로 실수 값을 나타내줌 (다른 언어들은 오류나는 경우 있음)
 # 몫: //, 나머지: %
-# print(a+b)
-# print(a-b)
-# print(a*b)
-# print(a/b)
-# print(a//b)
-# print(a%b)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
 
 # 자료의 형변환
 a = 10
 b = 20
 # 결과값이 1020이 나오도록 덧셈을 하여라
-# print(str(a) + str(b))
+print(str(a) + str(b))
 c = 2.333333
 # c의 소수부분을 잘라서 출력하라
-# print(int(c))
+print(int(c))
