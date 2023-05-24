7faadb331214d896ecdade9479be9656e860267d
lolddong <w2crosss@gmail.com>
2023-05-24 17:08:29 +0900

    very good

--- if_statements.py
+++ if_statements.py
@@ -27,13 +27,18 @@
 #   for 변수 in 반복가능한자료형(iterable): #예) list, cell, etc.
 #       실행문
 # 예)
-lista = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-for 변수 in lista:
-    print(변수)
-    #     >> 
+    # lista = [10, 20, 30]
+    # for 변수 in lista:
+    #     print(변수)
+    #     >> 10
+    #     >> 20
+    #     >> 30
     # for a in [1, 3, 10]:
     #     print (a)
 
-# range 문법: range(x, y) x 이상 y 미만
-# for a in range(1, 101):
-    # print(a)
+    # range 문법: range(x, y) x 이상 y 미만
+    # for a in range(1, 101):
+        # print(a)
+
+for a in [1, 3, 10]:
+    print (a)
--- list_statements.py
+++ list_statements.py
@@ -20,20 +20,6 @@
 #   print(lista[3])
 #   print(lista[4])
 #   print(lista[5])
-
-# list안에 list의 값을 조회하는 방법
-    # list_ex1 = ['a', 'b', 'c', [1, 2, 3]]
-    # 1) 변수 number에 [1,2,3]을 담아 출력
-        # list_ex1 = ['a', 'b', 'c', [1, 2, 3]]
-        # number = list_ex1[-1]
-        # print(number)
-    # 2) number에 담은 값중 1과 3을 더해 4를 출력
-        # print(number[0]+number[2])
-
-# slicing
-    # 여러개의 데이터를 범위를 지정해서 가져오고 싶을 때 사용
-    # slicing의 결과값은 같은 list로 출력
-
 # 연습
     # 5번째까지 출력
     #   print(lista[:5])
@@ -42,3 +28,158 @@
     #   print(type(lista[:5]))
     #   >> <class 'list'>
 
+# list의 값을 조회
+    # list_ex1 = ['a', 'b', 'c', [1, 2, 3]]
+    # 1) 변수 number에 [1,2,3]을 담아 출력
+        # list_ex1 = ['a', 'b', 'c', [1, 2, 3]]
+        # number = list_ex1[-1]
+        # print(number)
+        # >>> [1, 2, 3]
+    # 2) number에 담은 값중 1과 3을 더해 4를 출력
+        # print(number[0]+number[2])
+        # >>> 4
+
+# slicing
+    # 여러개의 데이터를 범위를 지정해서 가져오고 싶을 때 사용
+    # slicing의 결과값은 같은 list로 출력
+
+# list 더하기
+    # lista = [1, 2, 3, 4, 5]
+    # listb = ['a', 'b', 'c']
+    # listc = lista + listb
+    # print(listc)
+    # >>> [1, 2, 3, 4, 5, 'a', 'b', 'c']
+
+# list 길이 구하기: len()
+    # lista = [1, 2, 3, 4, 5]
+    # print(len(lista))
+    # >> 5
+
+# list 값 수정
+    # 1개의 값만 바꿀 땐 1개의 값으로 대체
+        # lista = [1, 2, 3, 4, 5]
+        # lista[1] = 4
+        # print(lista)
+        # >> [1, 4, 3, 4, 5]
+    # 여러 값을 바꿀 땐 다른 list로 대체
+        # lista = [1, 2, 3, 4, 5]
+        # lista[2] = [5, 5, 5]
+        # print(lista)
+        # >> [1, 2, [5, 5, 5], 4, 5]
+# list 요소 개수 세기: count()
+    # list.count(값)
+        # 리스트 중 "1"이 몇 개 있는지 세보시오
+        # lista = ["1", "2", "3", "4", "1", "1", "3"]
+        # print(lista.count("1"))
+        # >> 3
+# list 값 삭제하기
+    # del 리스트명[인덱스] OR del 리스트명[시작:끝]
+        # 리스트 a = [1, 3, 5, 7, 9, 10] 에서 3번째 값을 삭제하라
+            # a = [1, 3, 5, 7, 9, 10]
+            # del a[2]
+            # print(a)
+            # >> [1, 3, 7, 9, 10]
+        # 리스트 a = [1, 3, 5, 7, 9, 10] 에서 2-5번째 값을 삭제하라
+            # a = [1, 3, 5, 7, 9, 10]
+            # del a[1:5]
+            # print(a)
+            # >> [1, 19]
+    # list.remove(값)
+        # 리스트 a = [1, 3, 5, 7, 9, 10] 에서 2번째 값 (3)을 삭제하라
+            # a = [1, 3, 5, 7, 9, 10]
+            # a.remove(3)
+            # print(a)
+            # >> [1, 5, 7, 9, 10]
+    # 연습문제
+        # 특정한 9라는 값을 모두 제거하기
+            # 방법 1
+                # lista = [1, 3, 9, 3, 5, 6, 9, 9]
+                # count = 0
+                # for a in range(0, len(lista)):
+                #     if lista[a-count] == 9:
+                #           del lista[a-count]
+                #           count = count + 1
+                # print(lista)
+            # 방법 2
+                # lista = [1, 3, 9, 3, 5, 6, 9, 9]
+                # listb = []
+                # for a in range(0, len(lista)):
+                #     if lista[a] !=9:
+                #           listb = listb + [lista[a]]
+                #           또는 listb.append(lista[a])
+                # print(listb)
+            # 방법 3
+                # lista = [1, 3, 9, 3, 5, 6, 9, 9]
+                # for a in range(0, len(lista)):
+                #     if 9 in lista:
+                #           lista.remove(9)
+                #     else:
+                #           break
+                # print(lista)
+# list 추가
+    # list.append() - 맨 뒤로 추가
+        # lista = [1, 3, 5, 7]
+        # 9를 append를 이용하여 추가해서 출력
+        # lista.append(9)
+        # print(lista)
+        # >> [1, 3, 5, 7, 9]
+        # list를 추가할 수 있음
+            # listb = [1, 2, 3]
+            # listc = ['a', 'b', 10]
+            # listb.append(listc)
+            # print(lista)
+            # >> [1, 2, 3, ['a', 'b', 10]]
+    # list.insert() - 위치를 지정하여 추가
+        # vowel = ['a', 'e', 'i', 'u']
+        # 'o'를 index 3 (4번째 자리에 추가하기)
+        # vowel.insert(3, 'o')
+        # print(vowel)
+        # >> ['a', 'e', 'i', 'o', 'u']
+    # list.extend() - iterable 객체를 list에 추가할 때 사용 - 각 요소를 꺼내어 맨 뒤에 추가
+        # lista = [1, 2, 3]
+        # listb = [10, 20, 30]
+        # lista.append(listb)
+        # print(lista)
+        # >> [1, 2, 3, 10, 20, 30]
+# list 정렬
+    # list.sort() - 오름차순 정렬 - 숫자, 영어, 한국어 다 됨
+        # numa = [1, 10, 2, 5, 7, 4]
+        # numa.sort()
+        # print(numa)
+        # >> [1, 2, 4, 5, 7, 10]
+        # chlist = ['brad', 'john', 'anna']
+        # chlist.sort()
+        # print(chlist)
+    # list.sort() reverse = True 하면 내림차순 정렬
+# list 뒤집기
+    # list.reverse()
+        # chlist = ['brad', 'john', 'anna']
+        # chlist.reverse()
+        # print(chlist)
+# list 위치반환
+    # list.index(값)
+        # lista = ['안','녕','히','가']
+        # lista.index('가')
+        # print(lista.index('가'))
+        # >> 3
+# list 마지막 요소 끄집어내기
+    # list.pop(값) - remove and return last value
+        # lista = [1, 2, 3, 4, 5, 6, 7]
+        # lastValue = lista.pop()
+        # print(lastValue)
+        # >> 7
+# list <-> 문자열 만들기
+    # 문자 list를 문자열로 만들기
+lista = ["hello", "world", "python"]
+st1 = " "
+st2 = st1.join(lista)
+print(st2)
+    # 문자열을 문자 list로 만들기
+        # list()
+            # sta1 = "hello world python"
+            # print(list(sta1))
+            # >> ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', ' ', 'p', 'y', 't', 'h', 'o', 'n']
+        # str.split()
+            # sta1 = "hello world python" 
+            # print(sta1.split())
+            # >> ['hello', 'world', 'python']
