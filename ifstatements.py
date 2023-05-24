66759ac5b69f8532d594d230d8f12ae9a3aca15c
lolddong <w2crosss@gmail.com>
2023-05-24 10:00:22 +0900

    good

---
+++ ifstatements.py
@@ -0,0 +1,22 @@
+# if문의 기본 문법
+# if 참 조건:
+#    실행문
+# else:
+#    실행문
+# a = int(input("a는 무슨 숫자입니까?"))
+# if a>10:
+#     print("a는 10보다 큽니다.")
+# else: 
+#     print("a는 10보다 작습니다.")
+
+# trueOrFalse = True
+# if trueOrFalse:
+#     print("참입니다.")
+# else:
+#     print("거짓입니다.")
+
+a = int(input("얼마를 가지고 있습니까?"))
+if a > 30000:
+    print("택시를 타고 가시오.")
+else:
+    print("걸어가시오.")
