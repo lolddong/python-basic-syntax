988a3c4fc2f8675890ff9319a3d95f928b24e339
lolddong <w2crosss@gmail.com>
2023-06-01 10:40:22 +0900

    nightyyy

--- 13_module_import.py
+++ 13_module_import.py
@@ -7,7 +7,8 @@
     # from 패키지명 import 파일명
         # from module_test import 13_module)statements
 # 약어
-    # import 패키지명.파일명 as 약어
+    # as
+    # 예) import 패키지명.파일명 as 약어
         # import module_test.module_statements as ms
         # print(ms.plus(10, 20))
 # 예)
@@ -17,6 +18,8 @@
 
 import class_statements
 c1 = class_statements.CalculatorChild()
+
+# module명 만들 때 폴더의 모듈명은 숫자로 시작 가능하나 module을 import할 때에는 module명이 숫자로 시작할 수 없다
 
 c1.plus(100)
 print(f"modul_import의 result: {c1.result}")
---
+++ 16_web_crawling.py
@@ -0,0 +1,18 @@
+# while True:
+#     # https://로 시작하는 url html 정보 다 가져오기
+#     if aaa tag in alist:
+#         click
+#         while True:
+#             실행문
+
+# json: key value로 이루어져 있는 데이터 형식, 아직 정제되어있지 않은 데이터, {}로 감싸져있음
+ 
+# 아래 모듈은 외장 라이브러리이므로, 별도 설치 필요
+# 설치 시 pip라는 패키지tool을 이용 (python 설치시 동시에 pip설치된다)
+# terminal에 밑 중 하나 입력 후 enter:
+        # python3 --version 라고 밑 
+        # pip --version
+        # python -m pip --version
+
+import requests
+from bs4 import BeautifulSoup
